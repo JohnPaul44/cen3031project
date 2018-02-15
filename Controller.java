4abfa1041ba3bc8c3be4b40fee58a42a46517183
JohnPaul44 <jwpaul101@yahoo.com>
2018-02-15 14:42:47 -0500

    Controller Functionality

--- Controller.java
+++ Controller.java
@@ -1,9 +1,20 @@
 package registerUser;
 
+import javafx.fxml.FXML;
+
+import javax.xml.soap.Text;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    @FXML
+     private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+
     public void registerButtonClicked() {
-        System.out.println("Registered!!!");
+        System.out.println(usernameField.getText());
+        System.out.println(passwordField.getText());
     }
 }
