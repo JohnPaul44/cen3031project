9099624d16b906bdcb466149beb2c77aa136c42e
Thomas Headley <thomasheadleyjr@gmail.com>
2018-02-19 12:19:41 -0500

    Implemented Handler Factory

--- src/connection/ServerConnection.java
+++ src/connection/ServerConnection.java
@@ -3,9 +3,6 @@ package connection;
 import java.io.*;
 import java.net.*;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import connection.notificationMessageHandlers.*;
 import model.CurrentUser;
 
@@ -37,79 +34,16 @@ public class ServerConnection {
     public void startListeningToServer() {
         Thread thread = new Thread(() -> {
             String userInput;
+            HandlerFactory handlerFactory = new HandlerFactory();
             try {
                 while ((userInput = in.readLine()) != null) {
-                    Gson gson = new Gson();
-                    JsonParser parser = new JsonParser();
-                    JsonObject messageFromServer = parser.parse(userInput).getAsJsonObject();
-                    int status = messageFromServer.get("status").getAsInt();
-                    System.out.println(status);
-
-                    switch (status) {
-                        case 0: // Uninitialized
-                            UninitializedMessageHandler uninitializedHandler = new UninitializedMessageHandler();
-                            uninitializedHandler.handle();
-                            break;
-                        case 1: // Error Message
-                            ErrorMessageHandler errorHandler = new ErrorMessageHandler(messageFromServer);
-                            errorHandler.handle();
-                            break;
-                        case 2: // Logged In Message
-                            NotificationMessageHandler loggedInMessageHandler = new LoggedInMessageHandler(messageFromServer, currentUser);
-                            loggedInMessageHandler.handle();
-                            break;
-                        case 3: // User Online Status
-                            UserOnlineStatusMessageHandler userOnlineStatusMessageHandler = new UserOnlineStatusMessageHandler(messageFromServer, currentUser);
-                            userOnlineStatusMessageHandler.handle();
-                            break;
-                        case 4: // Logged Out Message
-                            LoggedOutMessageHandler loggedOutMessageHandler = new LoggedOutMessageHandler(messageFromServer, currentUser);
-                            loggedOutMessageHandler.handle();
-                            break;
-                        case 5: // Contact Added
-                            ContactAddedMessageHandler contactAddedMessageHandler = new ContactAddedMessageHandler(messageFromServer, currentUser);
-                            contactAddedMessageHandler.handle();
-                            break;
-                        case 6: // Contact Removed
-                            ContactRemovedMessageHandler contactRemovedMessageHandler = new ContactRemovedMessageHandler(messageFromServer, currentUser);
-                            contactRemovedMessageHandler.handle();
-                            break;
-                        case 7: // Profile Updated
-                            ProfileUpdatedMessageHandler profileUpdatedMessageHandler = new ProfileUpdatedMessageHandler(messageFromServer, currentUser);
-                            profileUpdatedMessageHandler.handle();
-                            break;
-                        case 8: // Message Received
-                            MessageReceivedMessageHandler messageReceivedMessageHandler = new MessageReceivedMessageHandler(messageFromServer, currentUser);
-                            messageReceivedMessageHandler.handle();
-                            break;
-                        case 9: // Message Updated
-                            MessageUpdatedMessageHandler messageUpdatedMessageHandler = new MessageUpdatedMessageHandler(messageFromServer, currentUser);
-                            messageUpdatedMessageHandler.handle();
-                            break;
-                        case 10: // User Added to Conversation
-                            UserAddedToConversationMessageHandler userAddedToConversationMessageHandler = new UserAddedToConversationMessageHandler(messageFromServer, currentUser);
-                            userAddedToConversationMessageHandler.handle();
-                            break;
-                        case 11: // User Removed from Conversation
-                            UserRemovedFromConversationMessageHandler userRemovedFromConversationMessageHandler = new UserRemovedFromConversationMessageHandler(messageFromServer, currentUser);
-                            userRemovedFromConversationMessageHandler.handle();
-                            break;
-                        case 12: // Message Read
-                            MessageReadMessageHandler messageReadMessageHandler = new MessageReadMessageHandler(messageFromServer, currentUser);
-                            messageReadMessageHandler.handle();
-                            break;
-                        case 13: // Typing
-                            MessageTypingMessageHandler messageTypingMessageHandler = new MessageTypingMessageHandler(messageFromServer, currentUser);
-                            messageTypingMessageHandler.handle();
-                            break;
-                        default:
-                            System.out.println("Invalid message received. Status of message received: " + status);
-                    }
-
-                    System.out.println("\nClient Received: " + userInput);
+                    NotificationMessageHandler handler = handlerFactory.produce(userInput, currentUser);
+                    handler.handle();
                 }
             } catch (IOException e) {
                 System.out.println("Error while receiving a message from server: " + e);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
---
+++ src/connection/notificationMessageHandlers/HandlerFactory.java
@@ -0,0 +1,48 @@
+package connection.notificationMessageHandlers;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import connection.serverMessages.ServerMessage;
+import model.CurrentUser;
+
+public class HandlerFactory {
+
+    public NotificationMessageHandler produce(String serverMessage, CurrentUser currentUser) throws Exception {
+        JsonParser parser = new JsonParser();
+        JsonObject messageFromServer = parser.parse(serverMessage).getAsJsonObject();
+        int status = messageFromServer.get("status").getAsInt();
+
+        switch (status) {
+            case 0: // Uninitialized
+                return new UninitializedMessageHandler();
+            case 1: // Error Message
+                return new ErrorMessageHandler(messageFromServer);
+            case 2: // Logged In Message
+                return new LoggedInMessageHandler(messageFromServer, currentUser);
+            case 3: // User Online Status
+                return new UserOnlineStatusMessageHandler(messageFromServer, currentUser);
+            case 4: // Logged Out Message
+                return new LoggedOutMessageHandler(messageFromServer, currentUser);
+            case 5: // Contact Added
+                return new ContactAddedMessageHandler(messageFromServer, currentUser);
+            case 6: // Contact Removed
+                return new ContactRemovedMessageHandler(messageFromServer, currentUser);
+            case 7: // Profile Updated
+                return new ProfileUpdatedMessageHandler(messageFromServer, currentUser);
+            case 8: // Message Received
+                return new MessageReceivedMessageHandler(messageFromServer, currentUser);
+            case 9: // Message Updated
+                return new MessageUpdatedMessageHandler(messageFromServer, currentUser);
+            case 10: // User Added to Conversation
+                return new UserAddedToConversationMessageHandler(messageFromServer, currentUser);
+            case 11: // User Removed from Conversation
+                return new UserRemovedFromConversationMessageHandler(messageFromServer, currentUser);
+            case 12: // Message Read
+                return new MessageReadMessageHandler(messageFromServer, currentUser);
+            case 13: // Typing
+                return new MessageTypingMessageHandler(messageFromServer, currentUser);
+            default:
+                throw new Exception("Invalid message received. Status of message received: \" + status");
+        }
+    }
+}
