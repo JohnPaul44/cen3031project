e31a47fe791e385b60fd18c3e8f027e88622b2fe
samielynne <samieleboli@gmail.com>
2018-02-19 09:28:36 -0500

    necessary dependencies and imports for TestFx

---
+++ cen3031project/src/application/LoginTest.java
@@ -0,0 +1,16 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.testfx.framework.junit.*;
+
+
+public class LoginTest extends ApplicationTest{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
