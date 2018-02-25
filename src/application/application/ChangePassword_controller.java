11c9580a49480f2a3a7f781fc5e0896be6bf6e4f
samielynne <samieleboli@gmail.com>
2018-02-25 13:54:35 -0500

    added design elements and different visibility settings

--- src/application/application/ChangePassword_controller.java
+++ src/application/application/ChangePassword_controller.java
@@ -12,6 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class ChangePassword_controller {
@@ -27,6 +29,10 @@ public class ChangePassword_controller {
 	private TextField confPass;
 	@FXML
 	private Button backButton;
+	@FXML
+	private Button confirmButton;
+	@FXML
+	private Button changeButton;
 	@FXML
 	private Label status;
 	@FXML
@@ -61,10 +67,79 @@ public class ChangePassword_controller {
 		((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
     }
 	
+	//event handlers for both when the login button is pressed or when the enter key is used
+	@FXML
+	public void confirmIdentityEventKey(KeyEvent keyEvent) throws Exception{
+		if(keyEvent.getCode() == KeyCode.ENTER) {
+		//calls the same action that occurs when the button is pressed
+		ActionEvent aevent = new ActionEvent(keyEvent.getSource(), confirmButton);
+		//pass the keyEvent into the button action event
+		confirmIdentity(aevent);
+		}
+	}
+	
+	@FXML
+	public void confirmIdentity(ActionEvent event) throws Exception{
+		//check the validity of the phone number
+		if(/*checkPhone(phone.getText())*/!phone.getText().equals("1234567890")) {
+			status.setText("Incorrect Credentials");
+			return;
+		}
+		//check the security question answer
+		if(/*checkSecurity(answer.getText())*/!answer.getText().equals("ans")) {
+			status.setText("Incorrect Credentials");
+			return;
+		}
+		
+		confirmButton.setVisible(false);
+		newPass.setVisible(true);
+		confPass.setVisible(true);
+		changeButton.setVisible(true);
+	}
+	
+	@FXML
+	public void changePasswordEventKey(KeyEvent keyEvent) throws Exception{
+		if(keyEvent.getCode() == KeyCode.ENTER) {
+		//calls the same action that occurs when the button is pressed
+		ActionEvent aevent = new ActionEvent(keyEvent.getSource(), changeButton);
+		//pass the keyEvent into the button action event
+		changePassword(aevent);
+		}
+	}
+	
+	@FXML
+	public void changePassword(ActionEvent event) throws Exception{
+		//check that the two passwords match
+		if(confPassword()) {
+			//send the information to the database
+		}
+		
+		//reopens the main login screen
+		BackButton(event);
+	}
+	
 	@FXML
 	public boolean confPassword() {
 		if(!newPass.getText().equals(confPass.getText())) {
 			status.setText("Passwords do not match");
+			return false;
+		}
+		return true;
+	}
+	
+	@FXML
+	public boolean checkPhone(String number) {
+		if(/*number is not associated with the username*/true){
+			status.setText("Incorrect phone number");
+			return false;
+		}
+		return true;
+	}
+	
+	@FXML
+	public boolean checkSecurityQuestion(String ans) {
+		if(/*answers do not match*/true) {
+			status.setText("Incorrect information");
 			return false;
 		}
 		return true;
