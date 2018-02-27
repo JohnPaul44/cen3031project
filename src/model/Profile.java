91314ab41965cc9d6341d6a2787bf0e58ad542b6
samielynne <samieleboli@gmail.com>
2018-02-27 17:16:46 -0500

    adding in setters for editing the profile

--- src/model/Profile.java
+++ src/model/Profile.java
@@ -54,4 +54,23 @@ public class Profile {
     public String getBirthday() {
         return birthday;
     }
+    public void setName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    public void setBirthday(String birthday){
+        this.birthday = birthday;
+    }
+
 }
