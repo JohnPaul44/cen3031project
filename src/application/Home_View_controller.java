6f27b0f2c35113ba58d0677bdb9bad7de6736204
samielynne <samieleboli@gmail.com>
2018-03-18 12:50:18 -0400

    functionality to open the direct messages and profile of users

--- src/application/Home_View_controller.java
+++ src/application/Home_View_controller.java
@@ -6,6 +6,7 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
@@ -13,11 +14,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Contact;
 import model.Profile;
 
+import java.awt.event.KeyEvent;
 import java.time.LocalDate;
 import java.util.HashMap;
 
@@ -44,12 +48,15 @@ public class Home_View_controller extends ViewController{
     public void setConversationsList(){
         HashMap<String, Contact> contactList = connection.getCurrentUser().getContactList();
 
-        if(!contactList.isEmpty()){
-            for(Contact value: contactList.values()){
-                createNewContact(value.getUsername());
-            }
-        }
+//        if(!contactList.isEmpty()){
+//            for(Contact value: contactList.values()){
+//                createNewContact(value.getUsername());
+//            }
+//        }
 
+        createNewContact("testing");
+        createNewContact("another");
+        createNewContact("one more");
         //TODO: import current conversations
     }
 
@@ -62,11 +69,27 @@ public class Home_View_controller extends ViewController{
         Label dm = new Label("Direct Message");
         dm.setCursor(Cursor.HAND);
         dm.setLabelFor(directMessage);
-        //TODO: set on mouse clicked action
+        dm.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try{
+                    OpenDirectMessage(event, user);
+                } catch(Exception e){}
+            }
+        });
+
         Label vp = new Label("View Profile");
         vp.setCursor(Cursor.HAND);
         vp.setLabelFor(viewProfile);
-        //TODO: set on mouse clicked action
+        vp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try{
+                    ViewOtherProfile(event, user);
+                } catch(Exception e){}
+            }
+        });
+
         content.setStyle("-fx-background-color: #E7DECD");
 
         content.getChildren().add(dm);
@@ -210,6 +233,79 @@ public class Home_View_controller extends ViewController{
             registerStage.show();
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void Search(javafx.scene.input.KeyEvent keyEvent) throws Exception{
+        if(keyEvent.getCode() == KeyCode.ENTER) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/application/search.fxml"));
+                loader.load();
+
+                Search_View_Controller searchScreen = loader.getController();
+                searchScreen.passConnection(connection);
+                searchScreen.setSearchField(search.getText());
+                connection.setDelegate(searchScreen);
+
+                Parent root = loader.getRoot();
+                Stage searchStage = new Stage();
+                Scene scene = new Scene(root, 600, 400);
+                scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+                searchStage.setScene(scene);
+                searchStage.show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void OpenDirectMessage(MouseEvent actionEvent, String user) throws Exception{
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/application/directMessage.fxml"));
+            loader.load();
+
+            Direct_Message_View_controller dmScreen = loader.getController();
+            dmScreen.passConnection(connection);
+            connection.setDelegate(dmScreen);
+            //TODO: pass the user to initialize the message screen
+
+            Parent root = loader.getRoot();
+            Stage dmStage = new Stage();
+            Scene scene = new Scene(root, 600, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            dmStage.setScene(scene);
+            dmStage.show();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void ViewOtherProfile(MouseEvent actionEvent, String user) throws Exception{
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            //TODO: change out the place holder fxml for view profile
+            loader.setLocation(getClass().getResource("/application/home.fxml"));
+            loader.load();
+
+            Home_View_controller vpScreen = loader.getController();
+            vpScreen.passConnection(connection);
+            vpScreen.setUsername(user);
+            connection.setDelegate(vpScreen);
+
+            Parent root = loader.getRoot();
+            Stage vpStage = new Stage();
+            Scene scene = new Scene(root, 600, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            vpStage.setScene(scene);
+            vpStage.show();
+        } catch(Exception e){
             e.printStackTrace();
         }
     }
