922827a8826fca505a92137b2c9e642be552a3f3
samielynne <samieleboli@gmail.com>
2018-03-27 10:32:41 -0400

    reverting

--- src/connection/notificationMessageHandlers/QueryResultsMessageHandler.java
+++ src/connection/notificationMessageHandlers/QueryResultsMessageHandler.java
@@ -6,18 +6,18 @@ import connection.serverMessages.ServerMessage;
 import connection.serverMessages.notificationMessages.NotificationQueryResultsMessage;
 
 public class QueryResultsMessageHandler implements MessageHandler {
-    private NotificationQueryResultsMessage queryResults;
+    private NotificationQueryResultsMessage serverMessage;
 
     public QueryResultsMessageHandler(ServerMessage messageFromServer) {
-        this.queryResults = (NotificationQueryResultsMessage) messageFromServer;
+        this.serverMessage = (NotificationQueryResultsMessage) messageFromServer;
     }
 
     @Override
     public void handle(ViewController delegate) {
         ErrorInformation errorInformation = new ErrorInformation();
-        if (queryResults.error()) {
-            errorInformation.setErrorInformation(queryResults);
+        if (serverMessage.error()) {
+            errorInformation.setErrorInformation(serverMessage);
         }
-        delegate.queryResultsNotification(errorInformation, queryResults.getResults());
+        delegate.queryResultsNotification(errorInformation, serverMessage.getResults());
     }
 }
