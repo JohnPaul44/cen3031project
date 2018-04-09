b60cbc64c1ed4d51edb5f5b7b1f1e398e746b23a
samielynne <samieleboli@gmail.com>
2018-04-09 09:32:57 -0400

    beginning of making group messages

--- src/application/CreateGroupMessage_View_Controller.java
+++ src/application/CreateGroupMessage_View_Controller.java
@@ -7,13 +7,11 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import model.Contact;
 
 import java.util.HashMap;
